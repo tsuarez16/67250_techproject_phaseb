a63fc684225b2468bdaa7bf799ecb561d8ef3ccd
Tate Suarez <tsuarez@andrew.cmu.edu>
2026-03-18 17:15:11 -0400

    increment 3

---
+++ tsuarez-increment3/static/script.js
@@ -0,0 +1,58 @@
+var x = 5;
+var y = 7;
+var z = x + y;
+console.log(z);
+
+var A = "Hello ";
+var B = "world!";
+var C = A + B;
+console.log(C);
+
+function sumnPrint(x1, x2) {
+    var result = x1 + x2;
+    console.log(result);
+}
+
+sumnPrint(x, y);
+sumnPrint(A, B);
+
+if (C.length > z) {
+    if (C.length < z) {
+        console.log(z);
+    }
+    console.log(C);
+} else if (C.length < z) {
+    console.log(z);
+} else {
+    console.log("good job!");
+}
+
+var L1 = ["Watermelon", "Pineapple", "Pear", "Banana"];
+var L2 = ["Apple", "Banana", "Kiwi", "Orange"];
+
+var now = new Date();
+var hour = now.getHours();
+
+function greeting(h) {
+    var greetingElement = document.getElementById("greeting");
+    if (greetingElement) {
+        if (h < 5 || h >= 20) {
+            greetingElement.innerHTML = "Good night! Welcome to MonoMuse";
+        } else if (h < 12) {
+            greetingElement.innerHTML = "Good morning! Welcome to MonoMuse";
+        } else if (h < 18) {
+            greetingElement.innerHTML = "Good afternoon! Welcome to MonoMuse";
+        } else {
+            greetingElement.innerHTML = "Good evening! Welcome to MonoMuse";
+        }
+    }
+}
+
+greeting(hour);
+
+function addYear() {
+    var yearElement = document.getElementById("copyYear");
+    if (yearElement) {
+        yearElement.innerHTML = "&copy; " + new Date().getFullYear() + " MonoMuse. All rights reserved.";
+    }
+}
